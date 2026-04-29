1f7f5e10e41569fe6b1b42e8293338379562e2e3
lizan <lizansarkar16@gmail.com>
2026-04-29 22:39:27 +0600

    add: practice abstraction inheritance practice complete

--- custom-type.js
+++ custom-type.js
@@ -1,8 +1,17 @@
-var user1;
 var users;
 users = [];
-user1 = { userName: "lizan", userId: 12 };
+var user1;
+user1 = { userName: 'anis', userId: 101 };
 users.push(user1);
-console.log(users);
-var GetRequest;
-GetRequest = "GET";
+var user2;
+user2 = { userName: 'rabu', userId: 102 };
+users.push(user2);
+var user3;
+user3 = { userName: 'lucky', userId: 103 };
+users.push(user3);
+var getRequest;
+getRequest = 'GET';
+function requestHandler(requestType) {
+    console.log(requestType);
+}
+requestHandler('GET');
--- custom-type.ts
+++ custom-type.ts
@@ -1,14 +1,27 @@
-type User = {userName: string; userId: number}
-let user1: User;
+type User = { userName: string, userId: number };
 
 let users: User[];
 users = [];
 
-user1 = {userName: "lizan", userId: 12}
+let user1: User;
+user1 = { userName: 'anis', userId: 101 };
 users.push(user1);
 
-console.log(users);
+let user2: User;
+user2 = { userName: 'rabu', userId: 102 };
+users.push(user2);
 
-type RequestType = "GET" | "POST";
-let GetRequest: RequestType;
-GetRequest = "GET";
+let user3: User;
+user3 = { userName: 'lucky', userId: 103 };
+users.push(user3);
+
+// console.log(users);
+
+type RequestType = 'GET' | 'POST';
+let getRequest: RequestType;
+getRequest = 'GET';
+
+function requestHandler(requestType: RequestType) {
+  console.log(requestType);
+}
+requestHandler('GET');
